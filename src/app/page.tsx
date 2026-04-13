799aac5596a9c57ccbecb18b496f19c4a53388a2
esureachroscielewska <agnieszka.chroscielewska@endava.com>
2026-04-13 11:24:12 +0200

    feat: add tracker to app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,19 @@
 import HeroSection from "./components/landing/hero-section";
 import FeaturesSection from "./components/landing/features-section";
 import HowItWorksSection from "./components/landing/how-it-works-section";
+import { tracker } from "@openreplay/tracker/cjs";
+import { useEffect } from "react";
+
+tracker.configure({
+  projectKey: "fKkLHVCGkQy0Hsp7PfiT",
+});
 
 export default function Home() {
+  useEffect(() => {
+    // use componentDidMount in case of React Class Component
+    tracker.start();
+  }, []);
+
   return (
     <main className="min-h-screen">
       <HeroSection />
